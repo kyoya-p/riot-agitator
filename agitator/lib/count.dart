3d2d483580e4591c9bf08ea93e12f9b4219176d8
kyoya <kyoya.p@gmail.com>
2020-12-09 08:10:32 +0900

    n

--- agitator/lib/count.dart
+++ agitator/lib/count.dart
@@ -20,39 +20,88 @@ void countTest() async {
   User user = FirebaseAuth.instance.currentUser;
   //print(user); //TODO
   FirebaseFirestore db = FirebaseFirestore.instance;
-  Query targetItems = db.collection("tmp").orderBy("time");
+  Query targetItems = db.collection("device/KK1/logs").orderBy("time");
   CollectionReference summaryCollection = db.collection("tmpCount");
-  makeCountSummary(targetItems, summaryCollection, "time",
-      Timestamp.fromMillisecondsSinceEpoch(15 * 1000), 1);
+  makeCountSummary(
+    targetItems,
+    summaryCollection,
+    "time",
+    DateTime.now().toUtc().millisecondsSinceEpoch,
+    1000,
+  );
 }
 
-Future<int> makeCountSummary(
+makeCountSummary(
   Query targetItems,
   CollectionReference summaryCollection,
   String timeField,
-  Timestamp time,
+  int time,
+  int resolutionInSecond,
+) async {
+  int totalCount = 0;
+  int lastTotalCount = 0;
+  int lastTime = 0;
+
+  await listItems(targetItems, timeField, time, (e) {
+    int t = e[timeField] ~/ resolutionInSecond * resolutionInSecond;
+    if (t != lastTime) {
+      //Count Fix
+      CountSegment seg = CountSegment(lastTime, t - lastTime,
+          totalCount: totalCount, count: totalCount - lastTotalCount);
+      lastTotalCount = totalCount;
+      print(seg);
+      summaryCollection.doc("${seg.since}:${seg.period}").set(seg.toMap());
+      lastTime = t;
+    } else {
+      print(totalCount);
+    }
+    totalCount = totalCount + 1;
+  });
+  if (time ~/ resolutionInSecond != lastTime) {
+    print(CountSegment(
+        lastTime, (time ~/ resolutionInSecond * resolutionInSecond) - lastTime,
+        totalCount: totalCount, count: totalCount - lastTotalCount));
+  }
+}
+
+/*Future<int> makeCountSummary_X1(
+  Query targetItems,
+  CollectionReference summaryCollection,
+  String timeField,
+  int time,
   int minResolutionInSecond,
 ) async {
-  Map<int, CountSegment> currentCountSegments = {};
+  List<CountSegment> currentCountSegs = List.from([], growable: true);
 
   listItems(targetItems, timeField, time, (e) {
     int t = e[timeField]; //TODO
-    List<CountSegment> segs = makeSegments(t);
-    String r = segs.map((e) => "${e.since}~${e.period}:${e.count}").join(",");
-    print("$t: $r");
+    List<CountSegment> newCountSegs = makeSegments(t);
 
-    for (e in segs) {
-      if (currentCountSegments[e.period] == null) {}
+    for (int i = 0; i < newCountSegs.length; ++i) {
+      if (currentCountSegs.length <= i) currentCountSegs.add(newCountSegs[i]);
+      if (currentCountSegs[i].since == newCountSegs[i].since) {
+        currentCountSegs[i].totalCount = currentCountSegs[i].totalCount + 1;
+      } else {
+        var e = currentCountSegs[i];
+        print("[${e.since}~${e.period}:${e.totalCount}]");
+        currentCountSegs[i] = newCountSegs[i]..totalCount = 1;
+      }
     }
+    String r = currentCountSegs
+        .map((e) => "${e.since}~${e.period}:${e.totalCount}")
+        .join(",");
+    print("$t: $r");
   });
 
   return 0;
 }
 
-listItems(Query targetItems, String timeField, Timestamp time,
-    op(DocumentSnapshot)) async {
+ */
+
+listItems(
+    Query targetItems, String timeField, int time, op(DocumentSnapshot)) async {
   QuerySnapshot logs =
-      await targetItems.where(timeField, isLessThan: time.seconds).get();
+      await targetItems.where(timeField, isLessThan: time).get();
   for (DocumentSnapshot e in logs.docs) {
     op(e);
   }
@@ -63,14 +112,24 @@ List<CountSegment> makeSegments(int time /*[sec]*/) {
     for (int period = 1; period <= time; period *= 2)
       CountSegment(time ~/ period * period, period)
   ];
-
   return list;
 }
 
 class CountSegment {
-  CountSegment(this.since, this.period);
+  CountSegment(this.since, this.period, {this.totalCount = 0, this.count = 0});
 
   final int since;
   final int period;
-  int count = 0;
+  final int totalCount;
+  final int count;
+
+  String toString() => "${since}~${period}:${totalCount}(+$count)";
+
+  Map<String, dynamic> toMap() => {
+        "since": since,
+        "period": period,
+        "totalCount": totalCount,
+        "count": count,
+        "type": "test",
+      };
 }
